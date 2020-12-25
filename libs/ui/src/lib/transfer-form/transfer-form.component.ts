9ac966151b622e9055522ed93ebb059b3499393f
Salama Ashoush <salamaashoush@gmail.com>
2020-12-25 08:38:03 +0100

    fix from account

--- libs/ui/src/lib/transfer-form/transfer-form.component.ts
+++ libs/ui/src/lib/transfer-form/transfer-form.component.ts
@@ -62,7 +62,7 @@ export class TransferFormComponent implements OnInit, OnChanges {
   handleTransfer() {
     this.transfer.emit(this.transferForm.value);
     this.transferForm.enable();
-    this.transferForm.get('toAccount').disable();
+    this.transferForm.get('fromAccount').disable();
     this.submitted = false;
     this.transferForm.reset({ fromAccount: accountToString(this.account) });
   }
